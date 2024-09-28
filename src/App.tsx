1a67a02898b16245ef6b15bdd1988ca0213e30a4
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-09-28 23:01:47 +0200

    feat: placeholders + basic colors

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
+
 import {
   Form,
   FormControl,
@@ -42,78 +43,80 @@ function App() {
   }
 
   return (
-    <div className="bg-primary">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="initialDeposit"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Initial deposit</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="periodicalContributionAmout"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Periodical contribution amout</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="investmentFrequency"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Investment frequency</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="anualInterestRate"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Anual interest rate</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="duration"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Investment duration</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    </div >
+    <body className="bg-primary h-screen">
+      <section className="bg-card h-screen w-2/5">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="initialDeposit"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-card-foreground">Initial deposit</FormLabel>
+                  <FormControl>
+                    <Input className="text-card-foreground" placeholder="1000" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="periodicalContributionAmout"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Periodical contribution amout</FormLabel>
+                  <FormControl>
+                    <Input placeholder="100" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="investmentFrequency"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Investment frequency</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="anualInterestRate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Anual interest rate</FormLabel>
+                  <FormControl>
+                    <Input placeholder="1.000" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="duration"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Investment duration</FormLabel>
+                  <FormControl>
+                    <Input placeholder="2" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant="outline" type="submit" className='bg-primary'>Submit</Button>
+          </form>
+        </Form>
+      </section >
+    </body>
   )
 }
 
